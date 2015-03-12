2654ccf8be82ec6b93074a0c7d3eefb8182abe7c
nautilus28 <nautilus404@gmail.com>
2015-03-12 10:36:06 +0700

    change port

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ mongoose.connect('localhost/lion_crawlers');
 
 var app = express();
 
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 6969);
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
